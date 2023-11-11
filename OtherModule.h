ec9f16e5a14631f58632768757e4807d275763e3
김하연 <vthyrachel@naver.com>
2023-11-11 16:35:07 +0900

    Update OtherModule.h description

--- OtherModule.h
+++ OtherModule.h
@@ -9,6 +9,8 @@
 #include "MacroAndGlobal.h"
 #include "OtherModule.h"
 
+// 이거 그냥 다른 사람 모듈에 있는거 내가 필요해서 빼놨어
+// SetCurrentCursorPos 작성한 사람은 이 파일 지워줘!
 void SetCurrentCursorPos(int x, int y) {}
 
 #endif
